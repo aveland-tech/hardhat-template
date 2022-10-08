8e2bb134c2b8d8fc7e9fda808c2de9c00cbaf3d0
kacamifici <kacamifici@gmail.com>
2022-10-08 10:02:39 +0200

    Initial template for solidity contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,42 +1,75 @@
 import * as dotenv from "dotenv";
 
-import { HardhatUserConfig, task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
+import "hardhat-abi-exporter";
+import "hardhat-contract-sizer";
+import "@primitivefi/hardhat-dodoc";
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+import "./tasks/accounts/account"
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
-  networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      metadata: {
+        bytecodeHash: "ipfs",
+      },
+      // You should disable the optimizer when debugging
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 490,
+      },
     },
   },
+  abiExporter: {
+    flat: true,
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
+    enabled: process.env.REPORT_GAS ? true : false,
     currency: "USD",
+    // coinmarketcap: process.env.REPORT_GAS_COINMARKETCAP_API_KEY
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  dodoc: {
+    runOnCompile: true,
+    exclude: ["**/node_modules/**"],
+    keepFileStructure: false,
+  },
+};
+
+
+config.networks = {
+  // ropsten: {
+  //   url: process.env.ROPSTEN_URL || "",
+  //   accounts:
+  //     process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+  // },
+  ...config.networks,
+  hardhat: {
+    chainId: 1337,
   },
 };
 
